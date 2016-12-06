3ac3cfbfe9ff7b2a7bc33a43af06c30157719f97
Mark Haines <mjark@negativecurvature.net>
2016-12-06 15:18:44 +0000

    Moar schema

--- roomserver.sql
+++ roomserver.sql
@@ -5,29 +5,54 @@ CREATE TABLE rooms (
     room_id text NOT NULL,
     -- The current state of the room.
     state_nid bigint NOT NULL,
-    -- List of new events that are not referenced by any event in the room.
-    -- Stored as a CBOR list of ints.
-    forward_edges bytea NOT NULL,
-    -- List of events not in the room that are referenced by non outlier events
-    -- Stored as a CBOR list of ints.
-    backward_edges bytea NOT NULL,
+    -- The current active contiguous regions.
+    -- There can be more than one active contiguous region if the room is
+    -- forked. A fork happens when a server sends us new events that cannot
+    -- be joined to this contiguous region. This should not happen under normal
+    -- operation.
+    -- New events can reference multiple contiguous regions. The event will be
+    -- assigned the lowest active contiguous region of its ancestors.
+    contiguous_region_nids bigint[] DEFAULT NULL,
     -- Is the room alive? A room is alive if this server is joined to the room.
     is_alive boolean NOT NULL,
     -- A room may only appear in this table once.
     UNIQUE (room_id)
 );
 
+-- A contiguous_regions is a chunk of the event graph for a room where all the
+-- events are connected. That is every event either has a "prev_event" in the
+-- region or is a "prev_event" of an event in the region.
+CREATE TABLE contiguous_regions (
+    -- Local numeric ID for the contiguous_region
+    contiguous_region_nid NOT NULL,
+    -- Back reference to the room_id this contiguous_region is for.
+    -- Unused in normal operation, but potentially useful for background work.
+    room_nid            bigint NOT NULL,
+    -- List of new events that are not referenced by any event in this region.
+    forward_edges bigint[] NOT NULL,
+    -- List of events in this contigous region that reference an event that is
+    -- not in this contiguous region.
+    backward_edges bigint[] NOT NULL
+);
+
+-- The events table holds the meta-data for each event. The
 CREATE TABLE events (
     -- Local numeric ID for the event. Postitive IDs are used for new events,
     -- negative IDs are used for backfilled events and outliers.
     event_nid           bigint NOT NULL PRIMARY KEY,
     -- Local numeric ID for the room the event is in.
     room_nid            bigint NOT NULL,
+    -- The contiguous_region this event belongs to.
+    -- This can be NULL if the event is an outlier.
+    contiguous_region_nid bigint DEFAULT NULL,
     -- The depth of the event in the room taken from the "depth" key of the
-    -- event JSON.
-    depth               bigint NOT NULL,
+    -- event JSON, corrected to be bigger than the "depth" of the events that
+    -- preceeded it.
+    corrected_depth     bigint NOT NULL,
     -- Local numeric ID for the state at the event.
     -- This is NULL if we don't know the state at the event.
+    -- If the state is not NULL this this event is part of the contiguous
+    -- part of the event graph
     -- Since many different events will have the same state we separate the
     -- state into a separate table.
     state_nid           bigint DEFAULT NULL,
@@ -43,8 +68,10 @@ CREATE TABLE events (
     UNIQUE (event_id)
 );
 
--- Create an index by depth on the contiguous portion of the event list.
-CREATE INDEX event_depth ON events (room_nid, depth, event_nid) WHERE state_nid IS NOT NULL;
+-- Create an index by depth on each contiguous portion of the event graph.
+CREATE INDEX event_depth
+    ON events (contiguous_region_nid, corrected_depth, event_nid)
+    WHERE contiguous_region_nid IS NOT NULL;
 
 -- Stores the JSON for each event. This kept separate from the main events
 -- table to keep the rows in the main events table small.
@@ -58,17 +85,26 @@ CREATE TABLE event_json (
     event_json text NOT NULL
 );
 
--- The state of a room before an event. The state is stored as a CBOR map of
--- (type -> state_key -> event_nid) in ``state_data`` and a list of other
--- state entries to union together with the ``state_data``.
+-- The state of a room before an event.
+-- Stored as a list of state_data entries stored in a separate table.
 CREATE TABLE state (
     -- Local numeric ID for the state.
     state_nid bigint NOT NULL PRIMARY KEY,
     -- Local numeric ID of the room this state is for.
     -- Unused in normal operation, but potentially useful for background work.
     room_nid bigint NOT NULL,
-    -- CBOR list of state_nids
-    state_parent_nids bytea NOT NULL,
+    -- How many times this is referenced in the events table.
+    reference_count bigint NOT NULL,
+    -- list of state_data_nids
+    state_data_nids bigint[] NOT NULL,
+);
+
+-- The state data map stored as CBOR map of (type -> state_key -> event_nid).
+CREATE TABLE state_data (
+    -- Local numeric ID for this state data.
+    state_data_nid bigint NOT NULL PRIMARY KEY,
+    -- How many times this is referenced in the state table.
+    reference_count bigint NOT NULL,
     -- CBOR map of (type -> state_key -> event_nid)
     state_data bytea NOT NULL
 );
